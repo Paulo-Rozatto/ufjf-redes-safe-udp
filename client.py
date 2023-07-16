8ebd57de64620a07058f670f8819aa5402c3f293
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-07-16 14:15:48 -0300

    Adcionando taxa de erro

--- client.py
+++ client.py
@@ -3,15 +3,11 @@ import struct
 from package import Package, TYPE
 from constants import *
 
-TIME_OUT = 10000
-
-msgClient = "Hello UDP Server"
-msgEncoded = str.encode(msgClient)
-start, end = 0, 0
-receiverWindow = 1
+receiverWindow = DEFAULT_WINDOW_SIZE
+start, end, ack_count = 0, 0, 0
 
 file = []
-for i in range(0, 15):
+for i in range(0, 100000):
     file.append(Package(TYPE["DATA"], i, 0, receiverWindow, str(i)))
 
 # Criando um socket UDP do lado do cliente
@@ -26,24 +22,28 @@ def send_package(package):
     udp_packet = udp_header + msgEncoded
     udp_socket.sendto(udp_packet, (SERVER_ADDRESS, SERVER_PORT))
 
-
 while True:
-    if end >= len(file):
-        print("Closing connection")
+    if end >= len(file) and ack_count >= len(file):
+        # print("Sending FIN to close")
         package = Package(TYPE["FIN"], 0, 0, 0, "")
         send_package(package)
-        break
-
+        udp_socket.settimeout(TIME_OUT_CLIENT)
+        udp_packet, sender_address = udp_socket.recvfrom(PACKAGE_SIZE)
+        package = Package(bytes=udp_packet[16:])
+        if (package.type == TYPE["FIN"]):
+            # print("Received FIN {}".format(package))
+            break
+        continue
     
     while (end - start) < receiverWindow and end < len(file):
         package = file[end]
-        print("Sending package {}".format(package.seq_number))
+        # print("Sending package {}".format(package.seq_number))
         package.window_size = receiverWindow
         send_package(package)
         end += 1
 
     try:
-        udp_socket.settimeout(TIME_OUT)
+        udp_socket.settimeout(TIME_OUT_CLIENT)
         udp_packet, sender_address = udp_socket.recvfrom(PACKAGE_SIZE)
         udp_header = struct.unpack("!IIII", udp_packet[:16])
         udp_data = udp_packet[16:]
@@ -53,19 +53,20 @@ while True:
         msg = "Server message: {}\nValid checksum: {}".format(
             package, correct_checksum == package.checksum()
         )
-        print(msg)
+        # print(msg)
 
-        if package.type == TYPE["NAK"]:
-            print("Received NAK {}".format(package.ack_number))
+        if package.type == TYPE["NAK"] and package.ack_number < len(file):
+            # print("Received NAK {}".format(package.ack_number))
             receiverWindow = package.window_size
             package = file[package.ack_number]
             send_package(package)
         else:
             print("Received ACK {}".format(package.ack_number))
-            start = max(package.ack_number, end)
+            start = package.ack_number # max(package.ack_number, end)
             end = start
             receiverWindow = package.window_size
-            print("New start: {}, new end: {}".format(start, end))
+            ack_count = max(ack_count, start)
+            # print("New start: {}, new end: {}".format(start, end))
     except socket.timeout:
         print("Timeout")
         break
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-
 import socket
 import struct
+import random
 from package import Package, TYPE
 from constants import *
+
+# random.seed(42)
+random.seed(123213)
 
 # Criar um datagrama de socket
 udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -10,62 +13,77 @@ udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_socket.bind((SERVER_ADDRESS, SERVER_PORT))
 
 file = []
-window_size = 1
+window_size = DEFAULT_WINDOW_SIZE
 window = [None] * window_size
 start, end = 0, 0
 
 def send_package(package):
     msgEncoded = package.encode()
     # Embala o pacote propio dentro de um pacote udp
-    udp_header = struct.pack("!IIII", SERVER_PORT, SERVER_PORT, len(msgEncoded), package.checksum())
+    udp_header = struct.pack(
+        "!IIII", SERVER_PORT, SERVER_PORT, len(msgEncoded), package.checksum()
+    )
     udp_packet = udp_header + msgEncoded
     udp_socket.sendto(udp_packet, address)
 
 def nextStart(array):
     for i in range(1, len(array)):
-        if (array[i] == None):
+        if array[i] == None:
             return start + i
     return start + window_size
 
 print("UDP server up and listening")
-while(True):
-    udp_packet, address = udp_socket.recvfrom(PACKAGE_SIZE)
-    udp_header = struct.unpack("!IIII", udp_packet[:16])
-    udp_data = udp_packet[16:]
-    correct_checksum = udp_header[3]
-    package = Package(bytes=udp_data)
+while True:
+    try:
+        udp_socket.settimeout(TIME_OUT_SERVER)
+        udp_packet, address = udp_socket.recvfrom(PACKAGE_SIZE)
+        udp_header = struct.unpack("!IIII", udp_packet[:16])
+        udp_data = udp_packet[16:]
+        correct_checksum = udp_header[3]
+        package = Package(bytes=udp_data)
+        isError = random.random() < ERROR_RATE
 
-    if (correct_checksum != package.checksum()): # send nak
-        print("Checksum error")
-        package = Package(TYPE["NAK"], 0, start, window_size)
-        continue
+        if correct_checksum != package.checksum() or isError:  # send nak
+            # print("Checksum error")
+            # print("Corrupted message: {}".format(package))
+            window_size = max(int(window_size / 2), 1)
+            package = Package(TYPE["NAK"], 0, start, window_size)
+            # print("Sending NAK {}".format(package.ack_number))
+            send_package(package)
+            continue
 
-    print("Received message: {}".format(package))
+        # print("Received message: {}".format(package))
 
-    if (package.type == TYPE["FIN"]):
-        print("Closing connection")
-        print("File: {}".format([str(x.data) for x in file]))
+        if package.type == TYPE["FIN"]:
+            # print("Closing connection")
+            print("File: {}".format([str(x.data) for x in file]))
+            package = Package(TYPE["FIN"], 0, 0, 0, "")
+            send_package(package)
+            break
+
+        if package.seq_number == start:
+            # print("Added package to file")
+            new_start = nextStart(window)
+            file.append(package)
+            for i in range(start + 1, new_start):
+                file.append(window[i - start])
+            start = new_start
+            window_size = min(window_size + 1, MAX_WINDOW_SIZE)
+            window = [None] * window_size
+            package = Package(TYPE["ACK"], 0, start, window_size)
+            print("Sending ACK {}".format(package.ack_number))
+            send_package(package)
+        elif None not in window[1:]:
+            print("Window is full")
+            window_size = max(window_size / 2, 1)
+            package = Package(TYPE["NAK"], 0, start, window_size)
+            send_package(package)
+            continue
+        elif package.seq_number > start and package.seq_number < start + window_size:
+            # print("Added package to buffer, send nack for package {}".format(start))
+            window[package.seq_number - start] = package
+            package = Package(TYPE["NAK"], 0, start, window_size)
+            send_package(package)
+    except socket.timeout:
+        print("Timeout")
         break
-
-    if (package.seq_number == start):
-        print("Added package to file")
-        new_start = nextStart(window)
-        file.append(package)
-        for i in range(start + 1, new_start):
-            file.append(window[i - start])
-        start = new_start
-        window_size = min(window_size + 1, MAX_WINDOW_SIZE)
-        window = [None] * window_size
-        package = Package(TYPE["ACK"], 0, start, window_size)
-        send_package(package)
-    elif (None not in window[1:]):
-        print("Window is full")
-        window_size = max(window_size / 2, 1)
-        package = Package(TYPE["NAK"], 0, start, window_size)
-        send_package(package)
-        continue
-    elif (package.seq_number > start):
-        print("Added package to buffer, send nack for package {}".format(start))
-        window[package.seq_number - start] = package
-        package = Package(TYPE["NAK"], 0, start, window_size)
-        send_package(package)
